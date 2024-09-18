e496421d0ae568c5e2a8b0c8e3374db96856cb3e
Ivan Ausecha <ivanausecha5@gmail.com>
2024-09-17 19:42:16 -0500

    Correccion en la clase adder, half adder y full adder. Renombramiento de Listas en CCU y CCB. Funcional hasta el momento

--- src/main/scala/Circuitos/package.scala
+++ src/main/scala/Circuitos/package.scala
@@ -3,50 +3,48 @@ package object Circuitos {
     def crearChipBinario (funcion: (Int, Int) => Int): Chip = {
       // Dada la función que calcula una operación lógica binaria,
       // devuelve la representación del Chip respectivo
-      entrada => List(funcion(entrada.head, entrada(1)))
+      (chipList: List[Int]) => List(funcion(chipList.head, chipList.tail.head))
     }
     def crearChipUnario (funcion: Int => Int): Chip = {
       // Dada la función que calcula una operación lógica unaria (como la negación),
       // devuelve la representación del Chip respectivo
-      entrada => List(funcion(entrada.head))
+      (chipList: List[Int]) => List(funcion(chipList.head))
     }
     def half_adder: Chip = {
       // Devuelve el Chip correspondiente a un half adder
-      entrada => {
-        val a = entrada.head
-        val b = entrada(1)
-        val suma = a ^ b // XOR para la suma
-        val acarreo = a & b // AND para el acarreo
-        List(acarreo, suma)
-      }
+      (list: List[Int]) =>
+        if (list.head == 1 && list.head == list.tail.head) List(list.head, 0)
+        else List(0, list.head + list.tail.head)
     }
     def full_adder: Chip = {
       // Devuelve el Chip correspondiente a un full adder
-      entrada => {
-        val a = entrada.head
-        val b = entrada(1)
-        val carryIn = entrada(2)
-        val sumaIntermedia = a ^ b
-        val suma = sumaIntermedia ^ carryIn
-        val acarreo = (a & b) | (carryIn & sumaIntermedia)
-        List(acarreo, suma)
-      }
+      (list: List[Int]) =>
+        val firstHaResult = half_adder(List(list(1), list(2)))
+        val secondHaResult = half_adder(List(list.head, firstHaResult(1)))
+        val logicOr = crearChipBinario((x: Int, y: Int) => x + y - (x * y))
+        val cout = logicOr(List(secondHaResult.head, firstHaResult.head))
+        List(cout.head, secondHaResult(1))
     }
     def adder (n: Int): Chip = {
       // Devuelve un Chip que recibe una lista de 2n bits de entrada (los primeros n bits corresponden
       // al primer número a sumar y los segundos n bits corresponden al segundo número a sumar)
       // y devuelve n+1 bits de salida, donde el primero es el acarreo de la suma y los otros n
       // corresponden a los n bits de salida
-      entrada => {
-        val primerNumero = entrada.take(n) // Primer número de n bits
-        val segundoNumero = entrada.drop(n) // Segundo número de n bits
-        val resultado = (primerNumero zip segundoNumero).foldRight((0, List[Int]())) {
-          case ((bit1, bit2), (carryIn, acc)) =>
-            val sumaIntermedia = bit1 ^ bit2 ^ carryIn
-            val acarreo = (bit1 & bit2) | (carryIn & (bit1 ^ bit2))
-            (acarreo, sumaIntermedia :: acc)
+      def sumaBits(primerNumero: List[Int], segundoNumero: List[Int], carryIn: Int, acc: List[Int]): List[Int] = {
+        if (primerNumero.isEmpty) carryIn :: acc
+        else {
+          val bit1 = primerNumero.head
+          val bit2 = segundoNumero.head
+          val sumaIntermedia = bit1 ^ bit2 ^ carryIn
+          val nuevoAcarreo = (bit1 & bit2) | (carryIn & (bit1 ^ bit2))
+          sumaBits(primerNumero.tail, segundoNumero.tail, nuevoAcarreo, sumaIntermedia :: acc)
         }
-        List(resultado._1) ++ resultado._2 // Acarreo final seguido de la suma
+      }
+
+      List => {
+        val primerNumero = List.take(n)
+        val segundoNumero = List.drop(n)
+        sumaBits(primerNumero, segundoNumero, 0, Nil)
       }
     }
 }
